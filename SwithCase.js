5ff213edd970251459d708a9e88317950d7b8d60
HuyTranIT96 <63638051+HuyTranIT96@users.noreply.github.com>
2022-01-28 08:22:05 +0700

    Create SwithCase.js
    Switch-case

---
+++ SwithCase.js
@@ -0,0 +1,20 @@
+var date = 2;
+switch(date){
+    case 2:
+        console.log('hôm này là thứ 2');
+        break;
+    case 3:
+        console.log('hôm này là thứ 3');
+        break;
+    case 4:
+        console.log('hôm này là thứ 4');
+    case 5:
+        console.log('hôm này là thứ 5');
+        break;
+    default:
+        console.log('không biết');
+}
+
+// if else : cần tính đúng sai
+// switch case khi biết trước giá trị 
+//với số lượng case lớn 
