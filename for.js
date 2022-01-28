5968eccb54494166bfb879d50a7772406a67ff0b
HuyTranIT96 <63638051+HuyTranIT96@users.noreply.github.com>
2022-01-28 13:05:41 +0700

    Create for.js
    hi

---
+++ for.js
@@ -0,0 +1,13 @@
+var myArray= [
+    'Javescript',
+    'PHP',
+    'Java',
+    'Dart',
+    'Ruby',
+    'Python'
+];
+var arrayLength = myArray.length;
+
+for(var i = 0;i < arrayLength; i++){
+    console.log(myArray[i])
+}
