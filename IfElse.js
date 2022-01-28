8a419496279d972674ac47fec92e49fca19b267d
HuyTranIT96 <63638051+HuyTranIT96@users.noreply.github.com>
2022-01-28 08:12:05 +0700

    Create IfElse.js
    hi

---
+++ IfElse.js
@@ -0,0 +1,11 @@
+var date = 2;
+if(date === 2)
+{
+    console.log("hôm nay là thứ 2")
+} else if(data === 3){
+    console.log('Hôm nay là thứ 3')
+}else if(date === 4 ){
+    console.log('Hôm nay là thứ 4')
+}else{
+    console.log('mệt')
+}
