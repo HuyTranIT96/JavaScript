d0a182dc0282cee1e69c7894b70e3d077c076a02
HuyTranIT96 <63638051+HuyTranIT96@users.noreply.github.com>
2022-01-30 11:51:31 +0700

    while
    while

---
+++ while.js
@@ -0,0 +1,22 @@
+// loop
+var i = 0;
+while(i < 1000)
+{
+    i++;
+    console.log(i);
+}
+
+
+//  loop in array
+
+var myArray= [
+    'Javascript',
+    'PHP',
+    'Ruby'
+]
+
+var i = 0;
+while(i <myArray.length){
+    console.log(myArray[i]);
+    i++;
+}
