5aed4e9bac757f5dbec55ea446e0c300a0745547
HuyTranIT96 <63638051+HuyTranIT96@users.noreply.github.com>
2022-01-30 09:37:23 +0700

    For in
    For in

---
+++ For..In.js
@@ -0,0 +1,20 @@
+//object
+var myInfo  ={
+    name:'HuyTran',
+    age: '26',
+    address:'Hue, VietNam'
+}
+for(var key in myInfo){
+    console.log(myInfo[key]);
+}
+// array
+var Languge=[
+    'java',
+    'Javascript',
+    'C',
+    'C++'
+]
+for (var key in Languge){
+    console.log(key)
+}
+
