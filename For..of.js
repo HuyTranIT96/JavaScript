d396a86e02eec9065a66202ce0b2ecd026fe1203
HuyTranIT96 <63638051+HuyTranIT96@users.noreply.github.com>
2022-01-30 09:48:06 +0700

    for..of
    for..of

---
+++ For..of.js
@@ -0,0 +1,26 @@
+
+// array
+var Languge=[
+    'java',
+    'Javascript',
+    'C',
+    'C++'
+]
+for (var value of Languge){
+    console.log(value);
+}
+// string
+var Languge2='Javascript'
+for(var value of Languge2){
+    console.log(value);
+}
+//object
+var myInfo  ={
+    name:'HuyTran',
+    age: '26',
+    address:'Hue, VietNam'
+}
+for(var value of Object.values(myInfo)){
+    console.log(value);
+}
+
